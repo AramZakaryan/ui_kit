f560c8384b74692a587718d2ea2821fbc937997b
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-07-19 13:10:14 +0400

    interim Button component

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,3 +1,25 @@
-export const Button = () => {
-  return <button type={'button'}></button>
+import type { ComponentPropsWithoutRef } from 'react'
+
+import clsx from 'clsx'
+
+import s from './button.module.scss'
+
+export type ButtonProps = {
+  fullWidth?: boolean
+  variant?: 'primary' | 'secondary'
+} & ComponentPropsWithoutRef<'button'>
+
+export const Button = ({
+  className,
+  fullWidth,
+  variant = 'primary',
+  ...restProps
+}: ButtonProps) => {
+  return (
+    <button
+      className={clsx(s.button, s[variant], fullWidth && s.fullWidth, className)}
+      type={'button'}
+      {...restProps}
+    />
+  )
 }
